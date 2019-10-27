bbb9ff2a9c37d7e2a7c509b35ad18adb15efb464
nick theile <Nick@nicksmbp.local>
2019-10-27 01:39:34 -0500

    swapped redux-persist to filesystem

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,12 @@
 import './shim' // make sure to use es6 import and not require()
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
 // @ts-ignore
 import { name as appName } from './app.json';
+
+
+YellowBox.ignoreWarnings([
+    'Require cycle:',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
--- src/app/reduxStore/configureStore.ts
+++ src/app/reduxStore/configureStore.ts
@@ -4,16 +4,18 @@ import thunk from 'redux-thunk';
 import { State, reducer, initialState } from './index';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import FilesystemStorage from 'redux-persist-filesystem-storage'
 
 
 const composeEnhancers = composeWithDevTools({
     // Specify here name, actionsBlacklist, actionsCreators and other options
 });
 
+
 const persistConfig = {
     key: 'root',
-    storage
+    storage: FilesystemStorage,
+    debug: true
 }  
 const persistedReducer = persistReducer(persistConfig, reducer);
 
@@ -21,9 +23,9 @@ const persistedReducer = persistReducer(persistConfig, reducer);
  * We're giving State interface to create store
  * store is type of State defined in our reducers
  */
-const store = createStore<any, any, any, any>(
+const store = createStore(
     persistedReducer, 
-    initialState, 
+    undefined, 
     composeEnhancers(
         applyMiddleware(thunk)
     )
